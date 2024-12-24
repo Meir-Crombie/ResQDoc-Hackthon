8ef1856c0b2d95849cb83ff7e9f361dee10c0ff5
turhvjbufv <91851160+Turhvjbufv@users.noreply.github.com>
2024-12-24 17:01:05 +0200

    make a recording button and store recording in /data

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/pages/StartRecord.dart';
 import 'package:frontend/pages/home.dart';
 import 'package:frontend/pages/loading_screen.dart';
 import 'package:frontend/pages/paramedic_doc.dart';
@@ -9,7 +8,6 @@ void main() => runApp(MaterialApp(
       routes: {
         '/': (context) => Loading(),
         '/home': (context) => Home(),
-        '/location': (context) => ChooseLocation(),
         '/paramedic': (context) => ParamedicDoc(),
       },
     ));
--- frontend/lib/pages/home.dart
+++ frontend/lib/pages/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:record/record.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:path_provider/path_provider.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,6 +11,52 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final player = AudioPlayer();
+  final recorder = AudioRecorder();
+  bool isRecording = false;
+  String? filePath;
+  double currentPosition = 0;
+  double totalDuration = 0;
+  @override
+  void dispose() {
+    player.dispose();
+    recorder.dispose();
+    super.dispose();
+  }
+
+  Future<void> startRecording() async {
+    final bool isPermissionGranted = await recorder.hasPermission();
+    if (!isPermissionGranted) {
+      return;
+    }
+
+    final directory = await getApplicationDocumentsDirectory();
+    // Generate a unique file name using the current timestamp
+    String fileName = 'recording_${DateTime.now().millisecondsSinceEpoch}.m4a';
+    filePath = '${directory.path}/$fileName';
+
+    // Define the configuration for the recording
+    const config = RecordConfig(
+      // Specify the format, encoder, sample rate, etc., as needed
+      encoder: AudioEncoder.aacLc, // For example, using AAC codec
+      sampleRate: 44100, // Sample rate
+      bitRate: 128000, // Bit rate
+    );
+
+    // Start recording to file with the specified configuration
+    await recorder.start(config, path: filePath!);
+    setState(() {
+      isRecording = true;
+    });
+  }
+
+  Future<void> stopRecording() async {
+    final path = await recorder.stop();
+    setState(() {
+      isRecording = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,20 +68,29 @@ class _HomeState extends State<Home> {
         ),
         body: SafeArea(
             child: Column(children: <Widget>[
-          Center(
-            child: TextButton.icon(
-              onPressed: () {
-                Navigator.pushNamed(context, '/location');
-              },
-              icon: Icon(Icons.record_voice_over),
-              label: Text(
-                'התחל הקלטה',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                ),
-              ),
-            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              isRecording
+                  ? ElevatedButton(
+                      onPressed: stopRecording,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 30, vertical: 15),
+                      ),
+                      child: const Text('Stop'),
+                    )
+                  : ElevatedButton(
+                      onPressed: startRecording,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.red,
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 30, vertical: 15),
+                      ),
+                      child: const Text('Record'),
+                    ),
+            ],
           ),
           Center(
             child: TextButton.icon(
