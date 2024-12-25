d8c767d3247f97078fb55c944148a97ee012617e
daniel pilant <doubledan148@gmail.com>
2024-12-25 21:35:39 +0200

    home page design update

--- frontend/lib/pages/home.dart
+++ frontend/lib/pages/home.dart
@@ -31,7 +31,6 @@ class _HomeState extends State<Home> {
   @override
   void dispose() {
     player.dispose();
-    recorder.dispose();
     timer?.cancel();
     super.dispose();
   }
@@ -46,13 +45,7 @@ class _HomeState extends State<Home> {
     String fileName = 'recording_${DateTime.now().millisecondsSinceEpoch}.mp3';
     filePath = '${directory.path}/$fileName';
 
-    const config = RecordConfig(
-      encoder: AudioEncoder.aacLc,
-      sampleRate: 44100,
-      bitRate: 128000,
-    );
-
-    await recorder.start(config, path: filePath!);
+    await recorder.start(const RecordConfig(), path: filePath!);
 
     setState(() {
       isRecording = true;
@@ -70,13 +63,12 @@ class _HomeState extends State<Home> {
     await recorder.stop();
     timer?.cancel();
 
-    // שמירת פרטי ההקלטה בקובץ JSON
     try {
       final directory = await getApplicationDocumentsDirectory();
       final jsonFile = File('${directory.path}/recording_info.json');
 
       if (!await jsonFile.exists()) {
-        await jsonFile.create(recursive: true); // יצירת הקובץ אם לא קיים
+        await jsonFile.create(recursive: true);
       }
 
       final data = {'filePath': filePath, 'duration': secondsElapsed};
@@ -90,7 +82,6 @@ class _HomeState extends State<Home> {
       isRecording = false;
     });
 
-    // ניווט לעמוד תיעוד
     Navigator.pushNamed(
       context,
       '/paramedic',
@@ -100,6 +91,9 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 255, 123, 0),
@@ -109,9 +103,7 @@ class _HomeState extends State<Home> {
               Icons.home,
               color: Color.fromARGB(255, 0, 0, 0),
             ),
-            SizedBox(
-              width: 10,
-            ),
+            SizedBox(width: 10),
             Text(
               '',
               style:
@@ -119,7 +111,7 @@ class _HomeState extends State<Home> {
             ),
           ],
         ),
-        automaticallyImplyLeading: false, // ביטול כפתור המוביל הדיפולטי
+        automaticallyImplyLeading: false,
         actions: [
           Builder(
             builder: (BuildContext context) {
@@ -129,8 +121,7 @@ class _HomeState extends State<Home> {
                   color: Color.fromARGB(255, 0, 0, 0),
                 ),
                 onPressed: () {
-                  Scaffold.of(context)
-                      .openEndDrawer(); // פותח את ה-Drawer בצד ימין
+                  Scaffold.of(context).openEndDrawer();
                 },
               );
             },
@@ -190,157 +181,144 @@ class _HomeState extends State<Home> {
           ),
         ),
       ),
-      backgroundColor: const Color.fromARGB(255, 26, 199, 49),
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              const Color.fromARGB(255, 255, 255, 255),
-              const Color.fromARGB(255, 255, 255, 255),
-              const Color.fromARGB(255, 255, 191, 132)
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
+      backgroundColor: Colors.transparent,
+      body: SingleChildScrollView(
+        child: Container(
+          height: screenHeight,
+          width: screenWidth,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                const Color.fromARGB(255, 255, 255, 255),
+                const Color.fromARGB(255, 255, 191, 132)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
           ),
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              const SizedBox(height: 30),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const SizedBox(width: 20),
-                  SizedBox(
-                    width: 150,
-                    height: 150,
-                    child: Image.asset('assets/svgviewer-png-output.png'),
-                  ),
-                  SizedBox(
-                    width: 150,
-                    height: 150,
-                    child: Image.asset('assets/logo_ichud_2.png'),
-                  ),
-                  const SizedBox(width: 10),
-                ],
-              ),
-              const SizedBox(height: 30),
-              if (isRecording)
-                Text(
-                  formatTime(secondsElapsed),
-                  style: GoogleFonts.robotoMono(
-                    fontSize: 36,
-                    fontWeight: FontWeight.bold,
-                    color: const Color.fromARGB(255, 197, 0, 0),
-                    letterSpacing: 2,
-                  ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  height: screenHeight * 0.05,
                 ),
-              const SizedBox(height: 20),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(100),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Flexible(
+                      child: SizedBox(
+                        width: screenWidth * 0.4,
+                        child: Image.asset('assets/svgviewer-png-output.png'),
+                      ),
+                    ),
+                    Flexible(
+                      child: SizedBox(
+                        width: screenWidth * 0.4,
+                        child: Image.asset('assets/logo_ichud_2.png'),
+                      ),
+                    ),
+                  ],
                 ),
-                child: ElevatedButton(
+                SizedBox(height: screenHeight * 0.05),
+                if (isRecording)
+                  Text(
+                    formatTime(secondsElapsed),
+                    style: GoogleFonts.robotoMono(
+                      fontSize: screenWidth * 0.07,
+                      fontWeight: FontWeight.bold,
+                      color: const Color.fromARGB(255, 197, 0, 0),
+                      letterSpacing: 2,
+                    ),
+                  ),
+                SizedBox(height: screenHeight * 0.02),
+                ElevatedButton(
                   onPressed: isRecording ? stopRecording : startRecording,
                   style: ElevatedButton.styleFrom(
-                    foregroundColor: const Color.fromARGB(
-                        255, 0, 0, 0), // צבע המסגרת והאייקון
+                    padding: EdgeInsets.symmetric(
+                      horizontal: screenWidth * 0.15,
+                      vertical: screenWidth * 0.15,
+                    ),
                     backgroundColor: isRecording
-                        ? const Color.fromARGB(
-                            255, 197, 0, 0) // אדום מלא במצב לחוץ
-                        : const Color.fromARGB(
-                            255, 255, 255, 255), // שקוף במצב רגיל
-                    side: BorderSide(
-                      color:
-                          const Color.fromARGB(255, 197, 0, 0), // מסגרת אדומה
-                      width: 4, // עובי המסגרת
-                    ),
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 50, vertical: 50),
-                    textStyle: const TextStyle(
-                      fontSize: 30,
-                      fontFamily: 'David',
-                      fontWeight: FontWeight.bold,
-                    ),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(100),
-                    ),
+                        ? const Color.fromARGB(255, 197, 0, 0)
+                        : const Color.fromARGB(255, 255, 255, 255),
+                    shape: CircleBorder(),
                   ),
                   child: Icon(
-                    isRecording ? Icons.stop : Icons.mic,
-                    size: 100,
                     color: isRecording
                         ? const Color.fromARGB(255, 255, 255, 255)
                         : const Color.fromARGB(255, 197, 0, 0),
+                    isRecording ? Icons.stop : Icons.mic,
+                    size: screenWidth * 0.25,
                   ),
                 ),
-              ),
-              const SizedBox(height: 90),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/paramedic');
-                    },
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: const Color.fromARGB(255, 255, 123, 0),
-                      backgroundColor: Colors.white, // צבע הרקע
-                      side: BorderSide(
-                        color: const Color.fromARGB(
-                            255, 255, 123, 0), // מסגרת כתומה
-                        width: 2, // עובי המסגרת
+                SizedBox(height: screenHeight * 0.05),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/paramedic');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: const Color.fromARGB(255, 255, 123, 0),
+                        backgroundColor: Colors.white, // צבע הרקע
+                        side: BorderSide(
+                          color: const Color.fromARGB(
+                              255, 255, 123, 0), // מסגרת כתומה
+                          width: 2, // עובי המסגרת
+                        ),
+                        elevation: 0, // ללא צל
+                        shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.circular(30), // פינות מעוגלות
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 100, vertical: 15),
+                        textStyle: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
                       ),
-                      elevation: 0, // ללא צל
-                      shape: RoundedRectangleBorder(
-                        borderRadius:
-                            BorderRadius.circular(30), // פינות מעוגלות
-                      ),
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 100, vertical: 15),
-                      textStyle: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black),
+                      child: const Text('תיעוד'),
                     ),
-                    child: const Text('תיעוד'),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 40),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/studymode');
-                    },
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: const Color.fromARGB(255, 255, 123, 0),
-                      backgroundColor: Colors.white, // צבע הרקע
-                      side: BorderSide(
-                        color: const Color.fromARGB(
-                            255, 255, 89, 0), // מסגרת כתומה
-                        width: 2, // עובי המסגרת
+                  ],
+                ),
+                const SizedBox(height: 40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/studymode');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: const Color.fromARGB(255, 255, 123, 0),
+                        backgroundColor: Colors.white, // צבע הרקע
+                        side: BorderSide(
+                          color: const Color.fromARGB(
+                              255, 255, 89, 0), // מסגרת כתומה
+                          width: 2, // עובי המסגרת
+                        ),
+                        elevation: 0, // ללא צל
+                        shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.circular(30), // פינות מעוגלות
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 73, vertical: 15),
+                        textStyle: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
                       ),
-                      elevation: 0, // ללא צל
-                      shape: RoundedRectangleBorder(
-                        borderRadius:
-                            BorderRadius.circular(30), // פינות מעוגלות
-                      ),
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 73, vertical: 15),
-                      textStyle: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black),
+                      child: const Text('טופס מקוצר'),
                     ),
-                    child: const Text('טופס מקוצר'),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
