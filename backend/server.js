88332fb99d8f278c1d42117c4bfd88b33757f64f
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-12-25 14:03:35 +0200

    Multithreeding has been added and now the server supports multi requests

--- backend/server.js
+++ backend/server.js
@@ -8,46 +8,71 @@ import {
   formatModelResponse,
   logWithTimestamp,
 } from "./config/openAI.js";
+import cluster from "cluster";
+import os from "os";
 
 const port = process.env.PORT || 5000;
-const app = express();
+if (cluster.isMaster) {
+  // Master process
+  const numCPUs = os.cpus().length;
+  logWithTimestamp(`Master process running. Forking ${numCPUs} workers...`);
 
-//Configuration of the local storgae
-const storage = multer.diskStorage({
-  destination: "uploads/",
-  filename: function (req, file, cb) {
-    const timestamp = new Date().toISOString().replace(/[:.]/g, "-");
-    const random = crypto.randomBytes(4).toString("hex");
-    const originalName = file.originalname;
-    cb(null, `${timestamp}_${random}_${originalName}`);
-  },
-});
-const upload = multer({ storage: storage });
+  // Fork workers
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-logWithTimestamp(`Server starting in dev mode`);
+  // Handle worker crashes
+  cluster.on("exit", (worker, code, signal) => {
+    logWithTimestamp(
+      `Worker ${worker.process.pid} died. Forking new worker...`
+    );
+    cluster.fork();
+  });
 
-app.get("/", (req, res) => {
-  res.send("API Endpoint is served");
-});
+  // Log when worker is online
+  cluster.on("online", (worker) => {
+    logWithTimestamp(`Worker ${worker.process.pid} is online`);
+  });
+} else {
+  const app = express();
 
-// API Endpoint: POST Upload audio and initiate transcription
-app.post("/transcribe", upload.single("audio"), async (req, res) => {
-  try {
-    logWithTimestamp("=============== Transcribe Endpoint ===============");
-    logWithTimestamp("Step One: Getting audio file ...");
-    if (!req.file) {
-      return res.status(400).send("No audio file uploaded");
-    }
-    const audioFilePath = req.file.path;
+  //Configuration of the local storgae
+  const storage = multer.diskStorage({
+    destination: "uploads/",
+    filename: function (req, file, cb) {
+      const timestamp = new Date().toISOString().replace(/[:.]/g, "-");
+      const random = crypto.randomBytes(4).toString("hex");
+      const originalName = file.originalname;
+      cb(null, `${timestamp}_${random}_${originalName}`);
+    },
+  });
+  const upload = multer({ storage: storage });
 
-    logWithTimestamp("Step Two: Connecting to Whissper Model ...");
-    const client = getWhissperClient();
+  logWithTimestamp(`Server starting in dev mode`);
 
-    logWithTimestamp("Step Three: Processsing audio transcript ...");
-    const result = await client.audio.transcriptions.create({
-      model: "whisper",
-      language: "he",
-      prompt: `
+  app.get("/", (req, res) => {
+    res.send("API Endpoint is served");
+  });
+
+  // API Endpoint: POST Upload audio and initiate transcription
+  app.post("/transcribe", upload.single("audio"), async (req, res) => {
+    try {
+      logWithTimestamp("=============== Transcribe Endpoint ===============");
+      logWithTimestamp("Step One: Getting audio file ...");
+      if (!req.file) {
+        return res.status(400).send("No audio file uploaded");
+      }
+      const audioFilePath = req.file.path;
+
+      logWithTimestamp("Step Two: Connecting to Whissper Model ...");
+      const client = getWhissperClient();
+
+      logWithTimestamp("Step Three: Processsing audio transcript ...");
+      const result = await client.audio.transcriptions.create({
+        model: "whisper",
+        language: "he",
+        prompt: `
       You are provided with an hebrew audio conversation, transcript it into hebrew transcript.
       If the values are numbers such as אחת שתיים שלוש ארבע חמש שש שבע שמונה תשע עשר אפס transcript it to an regular number representation like 1 2 3 4 5 6 7 8 9 0 etc
       If the numbers are in a row, make sure to write it a single number rather single digits all by themselves
@@ -59,93 +84,107 @@ app.post("/transcribe", upload.single("audio"), async (req, res) => {
 
       If the numbers are not in a row, for example there is a stop between the digits and the conversation switched, present it as a digits and not as single number
       `,
-      file: createReadStream(audioFilePath),
-    });
+        file: createReadStream(audioFilePath),
+      });
 
-    logWithTimestamp("Step Four: Transcript succefully been generated ...");
-    logWithTimestamp("Step Five: Server responded");
-    res.json({ text: result.text });
-  } catch (err) {
-    logWithTimestamp("Error initiating transcription:", err.message);
-    res.status(500).send("Server Error");
-  }
-});
-
-app.post("/analyze", upload.single("audio"), async (req, res) => {
-  try {
-    logWithTimestamp("=============== Analyze Endpoint ===============");
-    if (!req.file) {
-      return res.status(400).send("No audio file uploaded");
+      logWithTimestamp("Step Four: Transcript succefully been generated ...");
+      logWithTimestamp("Step Five: Server responded");
+      res.json({ text: result.text });
+    } catch (err) {
+      logWithTimestamp("Error initiating transcription:", err.message);
+      res.status(500).send("Server Error");
     }
-    logWithTimestamp("Step One: Getting audio file ...");
-    // Only on production
-    const audioFilePath = req.file.path;
-    logWithTimestamp("Step Two: Connecting to Whissper Model ...");
-    const client = getWhissperClient();
+  });
 
-    logWithTimestamp("Step Three: Processsing audio transcript ...");
-    const resultText = await client.audio.transcriptions.create({
-      model: "whisper",
-      language: "he",
-      prompt: "Provide the text result in hebrew",
-      file: createReadStream(audioFilePath),
-    });
+  app.post("/analyze", upload.single("audio"), async (req, res) => {
+    try {
+      logWithTimestamp("=============== Analyze Endpoint ===============");
+      if (!req.file) {
+        return res.status(400).send("No audio file uploaded");
+      }
+      logWithTimestamp("Step One: Getting audio file ...");
+      // Only on production
+      const audioFilePath = req.file.path;
+      logWithTimestamp("Step Two: Connecting to Whissper Model ...");
+      const client = getWhissperClient();
 
-    logWithTimestamp("Step Four: Transcript succefully been generated ...");
-    const result = await getJsonFieldsFilled(resultText.text);
+      logWithTimestamp("Step Three: Processsing audio transcript ...");
+      const resultText = await client.audio.transcriptions.create({
+        model: "whisper",
+        language: "he",
+        prompt: "Provide the text result in hebrew",
+        file: createReadStream(audioFilePath),
+      });
 
-    logWithTimestamp(
-      "Step Eight: Formatting info data into proper JSON file ..."
-    );
-    const formattedResult = formatModelResponse(result);
+      logWithTimestamp("Step Four: Transcript succefully been generated ...");
+      const result = await getJsonFieldsFilled(resultText.text);
 
-    logWithTimestamp(
-      "Step Nine: Form data been converted into JSON succefully ..."
-    );
-    logWithTimestamp("Step Ten: Server responded");
-    res.setHeader("Content-Type", "application/json");
-    res.send(formattedResult);
-  } catch (err) {
-    logWithTimestamp("Error analyzing transcript:", err.message);
-    res.status(500).send("Server Error");
-  }
+      logWithTimestamp(
+        "Step Eight: Formatting info data into proper JSON file ..."
+      );
+      const formattedResult = formatModelResponse(result);
+
+      logWithTimestamp(
+        "Step Nine: Form data been converted into JSON succefully ..."
+      );
+      logWithTimestamp("Step Ten: Server responded");
+      res.setHeader("Content-Type", "application/json");
+      res.send(formattedResult);
+    } catch (err) {
+      logWithTimestamp("Error analyzing transcript:", err.message);
+      res.status(500).send("Server Error");
+    }
+  });
+
+  app.get("/analyzeDemo", async (req, res) => {
+    try {
+      logWithTimestamp("=============== Analyze Endpoint ===============");
+      logWithTimestamp("Step One: Getting audio file ...");
+      // Only on production
+      const audioFilePath = "./AudioFinal.m4a";
+      logWithTimestamp("Step Two: Connecting to Whissper Model ...");
+      const client = getWhissperClient();
+
+      logWithTimestamp("Step Three: Processsing audio transcript ...");
+      const resultText = await client.audio.transcriptions.create({
+        model: "whisper",
+        language: "he",
+        prompt: "Provide the text result in hebrew",
+        file: createReadStream(audioFilePath),
+      });
+
+      logWithTimestamp("Step Four: Transcript succefully been generated ...");
+      const result = await getJsonFieldsFilled(resultText.text);
+
+      logWithTimestamp(
+        "Step Eight: Formatting info data into proper JSON file ..."
+      );
+      const formattedResult = formatModelResponse(result);
+
+      logWithTimestamp(
+        "Step Nine: Form data been converted into JSON succefully ..."
+      );
+      logWithTimestamp("Step Ten: Server responded");
+      res.setHeader("Content-Type", "application/json");
+      res.send(formattedResult);
+    } catch (err) {
+      logWithTimestamp("Error analyzing transcript:", err.message);
+      res.status(500).send("Server Error");
+    }
+  });
+
+  app.listen(port, () =>
+    logWithTimestamp(`Server is running on port: ${port}`)
+  );
+}
+
+// Error handling
+process.on("uncaughtException", (error) => {
+  logWithTimestamp(`Uncaught Exception: ${error.message}`);
+  process.exit(1);
 });
 
-app.get("/analyzeDemo", async (req, res) => {
-  try {
-    logWithTimestamp("=============== Analyze Endpoint ===============");
-    logWithTimestamp("Step One: Getting audio file ...");
-    // Only on production
-    const audioFilePath = "./AudioFinal.m4a";
-    logWithTimestamp("Step Two: Connecting to Whissper Model ...");
-    const client = getWhissperClient();
-
-    logWithTimestamp("Step Three: Processsing audio transcript ...");
-    const resultText = await client.audio.transcriptions.create({
-      model: "whisper",
-      language: "he",
-      prompt: "Provide the text result in hebrew",
-      file: createReadStream(audioFilePath),
-    });
-
-    logWithTimestamp("Step Four: Transcript succefully been generated ...");
-    const result = await getJsonFieldsFilled(resultText.text);
-
-    logWithTimestamp(
-      "Step Eight: Formatting info data into proper JSON file ..."
-    );
-    const formattedResult = formatModelResponse(result);
-
-    logWithTimestamp(
-      "Step Nine: Form data been converted into JSON succefully ..."
-    );
-    logWithTimestamp("Step Ten: Server responded");
-    res.setHeader("Content-Type", "application/json");
-    res.send(formattedResult);
-  } catch (err) {
-    logWithTimestamp("Error analyzing transcript:", err.message);
-    res.status(500).send("Server Error");
-  }
+process.on("unhandledRejection", (reason, promise) => {
+  logWithTimestamp(`Unhandled Rejection at: ${promise}, reason: ${reason}`);
+  process.exit(1);
 });
-
-app.listen(port, () => logWithTimestamp(`Server is running on port: ${port}`));
