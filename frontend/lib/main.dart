68ad063de7a7b2116d9f01d3d87af7fb33965ae3
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-12-25 22:48:01 +0200

    Running as usual

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -14,22 +14,16 @@ Future<Map<String, dynamic>> readJson() async {
   return jsonDecode(response);
 }
 
-// void main() => runApp(MaterialApp(
-//       initialRoute: '/home',
-//       routes: {
-//         '/home': (context) => Home(),
-//         '/paramedic': (context) => ParamedicDoc(
-//               fileName: "",
-//             ),
-//         '/settings': (context) => Settings(),
-//         '/about': (context) => AboutPage(),
-//         '/past': (context) => MissionsPage(),
-//         '/studymode': (context) => StudyModePage(),
-//       },
-//     )); // Material app
-
-void main() {
-  runApp(MaterialApp(
-    home: SummeryScreeen(),
-  ));
-}
+void main() => runApp(MaterialApp(
+      initialRoute: '/home',
+      routes: {
+        '/home': (context) => Home(),
+        '/paramedic': (context) => ParamedicDoc(
+              fileName: "",
+            ),
+        '/settings': (context) => Settings(),
+        '/about': (context) => AboutPage(),
+        '/past': (context) => MissionsPage(),
+        '/studymode': (context) => StudyModePage(),
+      },
+    )); // Material app
