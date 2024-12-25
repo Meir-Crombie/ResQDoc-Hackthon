00b066530ad2d112a77fc1c1508cfa55f7c9179f
yitshac brody <173272111+diy-code@users.noreply.github.com>
2024-12-25 21:00:05 +0200

    updated the about_page

--- backend/pdf_report/main.py
+++ backend/pdf_report/main.py
@@ -2,12 +2,13 @@ import pdfkit
 from jinja2 import Environment, FileSystemLoader
 import os
 import json
+import sys
 
 # Function to create HTML from JSON data
 def create_html(data):
     env = Environment(loader=FileSystemLoader('.'))
     template = env.get_template('template.html')
-    return template.render(data=data)
+    return template.render(data=data) 
 
 # Function to generate PDF from HTML
 def generate_pdf(data, output_pdf):
@@ -30,6 +31,13 @@ def generate_pdf(data, output_pdf):
 
 # Example usage
 if __name__ == "__main__":
-    with open('storage/emulated/0/Documents/file.json', 'r', encoding='utf-8') as file:
+    if len(sys.argv) != 3:
+        print("Usage: python main.py <input_json_file> <output_pdf_file>")
+        sys.exit(1)
+
+    input_json_file = sys.argv[1]
+    output_pdf_file = sys.argv[2]
+
+    with open(input_json_file, 'r', encoding='utf-8') as file:
         json_data = json.load(file)
-    generate_pdf(json_data, 'report2.pdf')
+    generate_pdf(json_data, output_pdf_file)
