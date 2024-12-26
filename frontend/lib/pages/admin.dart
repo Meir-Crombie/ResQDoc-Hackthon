2f1939cb6b394a47d986bbd89df69456ef0fdf79
Meir Crombie <meircrombie@gmail.com>
2024-12-26 20:03:26 +0200

    remove unused import for past_doc.dart in admin.dart

--- frontend/lib/pages/admin.dart
+++ frontend/lib/pages/admin.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:frontend/pages/past_doc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'history_for_maneger.dart';
 
