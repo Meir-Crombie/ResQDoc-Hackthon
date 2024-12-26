aa121e3447eda5770e9b31f11676f48756dbc0bc
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-12-26 04:47:33 +0200

    The server has been cleaned and contains only speech to text and analiss with audio

--- backend/server.js
+++ backend/server.js
@@ -63,7 +63,7 @@ app.post("/transcribe", upload.single("audio"), async (req, res) => {
       `,
       file: createReadStream(audioFilePath),
     });
-
+    console.log(result);
     logWithTimestamp("Step Four: Transcript succefully been generated ...");
     logWithTimestamp("Step Five: Server responded");
     res.json({ text: result.text });
@@ -107,53 +107,7 @@ app.post("/analyze", upload.single("audio"), async (req, res) => {
 
     logWithTimestamp("Step Four: Transcript succefully been generated ...");
     const result = await getJsonFieldsFilled(resultText.text);
-    logWithTimestamp(
-      "Step Eight: Formatting info data into proper JSON file ..."
-    );
-    const formattedResult = formatModelResponse(result);
-
-    logWithTimestamp(
-      "Step Nine: Form data been converted into JSON succefully ..."
-    );
-    logWithTimestamp("Step Ten: Server responded");
-    res.setHeader("Content-Type", "application/json");
-    res.send(formattedResult);
-  } catch (err) {
-    logWithTimestamp("Error analyzing transcript:", err.message);
-    res.status(500).send("Server Error");
-  }
-});
-
-app.get("/analyzeDemo", async (req, res) => {
-  try {
-    logWithTimestamp("=============== Analyze Endpoint ===============");
-    logWithTimestamp("Step One: Getting audio file ...");
-    // Only on production
-    const audioFilePath = "./AudioFinal.m4a";
-    logWithTimestamp("Step Two: Connecting to Whissper Model ...");
-    const client = getWhissperClient();
-
-    logWithTimestamp("Step Three: Processsing audio transcript ...");
-    const resultText = await client.audio.transcriptions.create({
-      model: "whisper",
-      language: "he",
-      prompt: `
-      You are provided with an hebrew audio conversation, transcript it into hebrew transcript.
-      If the values are numbers such as אחת שתיים שלוש ארבע חמש שש שבע שמונה תשע עשר אפס transcript it to an regular number representation like 1 2 3 4 5 6 7 8 9 0 etc
-      If the numbers are in a row, make sure to write it a single number rather single digits all by themselves
-
-      here is an example
-      "אחת שתיים שלוש ארבע חמש שש שבע שמונה תשע"
-      Im expecting you to provide the transcript as:
-      "123456789"
-
-      If the numbers are not in a row, for example there is a stop between the digits and the conversation switched, present it as a digits and not as single number
-      `,
-      file: createReadStream(audioFilePath),
-    });
-
-    logWithTimestamp("Step Four: Transcript succefully been generated ...");
-    const result = await getJsonFieldsFilled(resultText.text);
+    console.log(result);
 
     logWithTimestamp(
       "Step Eight: Formatting info data into proper JSON file ..."
@@ -249,32 +203,4 @@ app.get("/showCase", async (req, res) => {
   }
 });
 
-app.post("/summary", express.json(), async (req, res) => {
-  try {
-    logWithTimestamp("=============== Summary Endpoint ===============");
-    console.log(req.body);
-    if (!req.body) {
-      return res.status(400).json({ error: "No JSON data provided" });
-    }
-
-    const jsonData = req.body;
-
-    if (!jsonData.response) {
-      return res.status(400).json({ error: "No 'response' field in JSON" });
-    }
-    logWithTimestamp("Step One: JSON data received and processed");
-    logWithTimestamp("Step Two: Transcript succefully been generated ...");
-    const result = await getSummeryFilled(jsonData);
-    const formattedResult = formatModelResponse(result);
-    logWithTimestamp(
-      "Step Eight: Formatting info data into proper JSON file ..."
-    );
-    logWithTimestamp("Step Ten: Server responded");
-    res.setHeader("Content-Type", "application/json");
-    res.send(formattedResult);
-  } catch (error) {
-    logWithTimestamp("Error processing JSON:", error.message);
-    res.status(500).json({ error: "Failed to process JSON data" });
-  }
-});
 app.listen(port, () => logWithTimestamp(`Server is running on port: ${port}`));
