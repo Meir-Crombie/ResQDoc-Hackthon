f03164856e695743a671e28f7679a578a2ff9354
Meir Crombie <meircrombie@gmail.com>
2024-12-26 07:17:09 +0200

    Add constructor to StudyModePage for improved widget initialization

--- frontend/lib/pages/Study_mode.dart
+++ frontend/lib/pages/Study_mode.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class StudyModePage extends StatelessWidget {
+  const StudyModePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
